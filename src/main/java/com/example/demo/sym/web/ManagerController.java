390b696e9a128a1e83c0cf636e225d19326cb9cf
elinwonyoungpark <elinpark.102@gmail.com>
2020-12-21 17:52:45 +0900

    2020-12-21

---
+++ src/main/java/com/example/demo/sym/web/ManagerController.java
@@ -0,0 +1,30 @@
+package com.example.demo.sym.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.demo.sym.service.ManagerService;
+import com.example.demo.sym.service.model.Manager;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/managers")
+public class ManagerController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired ManagerService managerService;
+    @PostMapping("")
+    public Map<?, ?> register(@RequestBody Manager manager) {
+        var map = new HashMap<>();
+        logger.info("등록하려는 관리자정보: "+manager.toString());
+        int result = managerService.register(manager);
+        map.put("message", (result == 1) ? "SUCCESS" : "FAILURE");
+        return map;
+    }
+   
+}
