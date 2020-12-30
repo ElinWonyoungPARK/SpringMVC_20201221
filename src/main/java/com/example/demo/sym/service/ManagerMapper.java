375e3b946491d18a8a10da56fa35fa0fba090a27
elinwonyoungpark <elinpark.102@gmail.com>
2020-12-30 18:03:48 +0900

    2020-12-30

--- src/main/java/com/example/demo/sym/service/ManagerMapper.java
+++ src/main/java/com/example/demo/sym/service/ManagerMapper.java
@@ -2,9 +2,10 @@ package com.example.demo.sym.service;
 
 import com.example.demo.sym.service.Manager;
 
-import org.springframework.stereotype.Repository;
 
-@Repository
+@FunctionalInterface
 public interface ManagerMapper {
+
 	public int insert(Manager manager);
+	
 }
